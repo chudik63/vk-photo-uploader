2f51fd22ab1b3e3014b2e15f76ea2a51b0482861
Vitalya <vitamin11182005@gmail.com>
2024-10-09 20:55:06 +0300

    add running server

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"vk-photo-storage/internal/infrastructure"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg, err := infrastructure.LoadConfig()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки конфигурации: %v", err)
+	}
+
+	router := gin.Default()
+
+	if err := router.Run(cfg.Server.Port); err != nil {
+		log.Fatalf("Ошибка запуска сервера: %v", err)
+	}
 }
