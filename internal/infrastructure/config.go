a3eb7e24b5f1f8fae4b4829cc1c64988058de664
Vitalya <vitamin11182005@gmail.com>
2024-10-09 20:49:54 +0300

    add config

---
+++ internal/infrastructure/config.go
@@ -0,0 +1,28 @@
+package infrastructure
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server struct {
+		Port string
+	}
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath("./configs")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
