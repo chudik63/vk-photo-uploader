c466bd9d9021de9fd8a97c879cd96feb83771bef
Vitalya <vitamin11182005@gmail.com>
2024-10-11 01:41:33 +0300

    add methods to set token and id in vk repository

--- internal/repository/vk_repository.go
+++ internal/repository/vk_repository.go
@@ -5,23 +5,31 @@ import "vk-photo-uploader/internal/entity"
 type VkRepository interface {
 	PhotoRepository
 	CreateAlbum(title string) error
+	SetToken(token string)
+	SetId(id int)
 }
 
 type vkRepository struct {
 	token string
+	id    int
 }
 
 func NewVkRepository() VkRepository {
-	return &vkRepository{
-		token: "",
-	}
+	return &vkRepository{}
 }
 
 func (r *vkRepository) Upload(photo *entity.Photo) error {
-
 	return nil
 }
 
 func (r *vkRepository) CreateAlbum(title string) error {
 	return nil
+}
+
+func (r *vkRepository) SetToken(token string) {
+	r.token = token
+}
+
+func (r *vkRepository) SetId(id int) {
+	r.id = id
 }
