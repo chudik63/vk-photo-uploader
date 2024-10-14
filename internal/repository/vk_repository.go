8c735cbf95e4c859562ce7076cb40043406855a1
Vitalya <vitamin11182005@gmail.com>
2024-10-14 23:00:22 +0300

    separate getAlbumId and readCaption methods & add new errors

--- internal/repository/vk_repository.go
+++ internal/repository/vk_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"mime/multipart"
@@ -76,12 +77,10 @@ func (r *VkRepository) Upload(path string) error {
 	wholePath, _ := os.Getwd()
 	wholePath = filepath.Join(wholePath, r.root, path)
 
-	ext := filepath.Ext(wholePath)
-	wholePathWithoutExt := strings.TrimSuffix(wholePath, ext)
-
-	wholePathTxt := wholePathWithoutExt + ".txt"
-
-	caption, err := os.ReadFile(wholePathTxt)
+	caption, err := r.readCaption(wholePath)
+	if err != nil {
+		return err
+	}
 
 	b := &bytes.Buffer{}
 	writer := multipart.NewWriter(b)
@@ -92,9 +91,14 @@ func (r *VkRepository) Upload(path string) error {
 	}
 	defer file.Close()
 
-	part, _ := writer.CreateFormFile("file1", filepath.Base(wholePath))
+	part, err := writer.CreateFormFile("file1", filepath.Base(wholePath))
+	if err != nil {
+		return err
+	}
 
-	io.Copy(part, file)
+	if _, err = io.Copy(part, file); err != nil {
+		return err
+	}
 
 	writer.Close()
 
@@ -103,14 +107,22 @@ func (r *VkRepository) Upload(path string) error {
 	req.Header.Set("Content-Type", writer.FormDataContentType())
 
 	client := &http.Client{}
-	resp, _ := client.Do(req)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
 
-	body, _ := io.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
 
 	msg := &postUrlResponse{}
-	json.Unmarshal(body, msg)
+	if err = json.Unmarshal(body, msg); err != nil {
+		return err
+	}
 
-	resp, err = http.Get(fmt.Sprintf("https://api.vk.com/method/photos.save?&album_id=%d&server=%d&photos_list=%s&hash=%s&caption=%s&access_token=%s&v=5.199", msg.Aid, msg.Server, msg.Photos_list, msg.Hash, url.QueryEscape(string(caption)), r.token))
+	resp, err = http.Get(fmt.Sprintf("https://api.vk.com/method/photos.save?&album_id=%d&server=%d&photos_list=%s&hash=%s&caption=%s&access_token=%s&v=5.199", msg.Aid, msg.Server, msg.Photos_list, msg.Hash, url.QueryEscape(caption), r.token))
 	if err != nil {
 		return err
 	}
@@ -120,6 +132,7 @@ func (r *VkRepository) Upload(path string) error {
 		return err
 	}
 
+	/// ПРОВЕРИТЬ загрузку
 	msg1 := &savePhotoResponse{}
 
 	err = json.Unmarshal(body, msg1)
@@ -130,31 +143,81 @@ func (r *VkRepository) Upload(path string) error {
 	return nil
 }
 
-func (r *VkRepository) getUploadServer(id int) (string, error) {
-	resp, err := http.Get(fmt.Sprintf("https://api.vk.com/method/photos.getUploadServer?&access_token=%s&album_id=%d&v=5.199", r.token, id))
+func (r *VkRepository) readCaption(path string) (string, error) {
+	ext := filepath.Ext(path)
+	wholePathWithoutExt := strings.TrimSuffix(path, ext)
+
+	wholePathTxt := wholePathWithoutExt + ".txt"
+
+	caption, err := os.ReadFile(wholePathTxt)
 	if err != nil {
 		return "", err
 	}
 
+	return string(caption), nil
+}
+
+func (r *VkRepository) getUploadServer(id int) (string, error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.vk.com/method/photos.getUploadServer?&access_token=%s&album_id=%d&v=5.199", r.token, id))
+	if err != nil {
+		return "", errors.New("ошибка получения сервера загрузки")
+	}
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return "", errors.New("нельзя считать ответ создания альбома")
 	}
 
 	msg := &getUploadServerResponse{}
 
 	err = json.Unmarshal(body, msg)
 	if err != nil {
-		return "", err
+		return "", errors.New("ошибка чтения JSON после получения сервера загрузки")
 	}
 	return msg.Response.UploadUrl, nil
 }
 
 func (r *VkRepository) createAlbum(title string) (int, error) {
-	resp, _ := http.Get(fmt.Sprintf("https://api.vk.com/method/photos.getAlbums?access_token=%s&v=5.199", r.token))
+	id, err := r.getAlbumId(title)
+	if err == nil {
+		return id, nil
+	}
+
+	resp, err := http.Get(fmt.Sprintf("https://api.vk.com/method/photos.createAlbum?title=%s&access_token=%s&v=5.199", title, r.token))
+	if err != nil {
+		return 0, errors.New("нельзя создать альбом VK")
+	}
+
 	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, errors.New("нельзя считать ответ создания альбома")
+	}
+
+	msg := &albumCreationResponse{}
+
+	err = json.Unmarshal(body, msg)
+	if err != nil {
+		return 0, errors.New("ошибка чтения JSON после создании альбома")
+	}
+
+	return msg.Response.Id, nil
+}
+
+func (r *VkRepository) getAlbumId(title string) (int, error) {
+	resp, err := http.Get(fmt.Sprintf("https://api.vk.com/method/photos.getAlbums?access_token=%s&v=5.199", r.token))
+	if err != nil {
+		return 0, errors.New("нельзя получить информацию о альбомах VK")
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, errors.New("нельзя считать ответ c информацией о альбомах")
+	}
+
 	msg := &getAlbumsResponse{}
-	json.Unmarshal(body, msg)
+	if err := json.Unmarshal(body, msg); err != nil {
+		return 0, errors.New("ошибка чтения JSON после проверки альбома")
+	}
 
 	for i := 0; i < msg.Response.Count; i++ {
 		if msg.Response.Array[i].Title == title {
@@ -162,24 +225,7 @@ func (r *VkRepository) createAlbum(title string) (int, error) {
 		}
 	}
 
-	resp, err = http.Get(fmt.Sprintf("https://api.vk.com/method/photos.createAlbum?title=%s&access_token=%s&v=5.199", title, r.token))
-	if err != nil {
-		return 0, err
-	}
-
-	body, err = io.ReadAll(resp.Body)
-	if err != nil {
-		return 0, err
-	}
-
-	msg1 := &albumCreationResponse{}
-
-	err = json.Unmarshal(body, msg1)
-	if err != nil {
-		return 0, err
-	}
-
-	return msg1.Response.Id, nil
+	return 0, errors.New("альбом не существует")
 }
 
 func (r *VkRepository) SetToken(token string) {
